03d417dc2f3625a05a5cababe06f756d3b09d2ae
yashpratap-mst <yash.singh@metasquaretech.com>
2025-01-20 16:29:55 +0530

    added a test static image

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const port = 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/static",express.static(path.join(__dirname, "public")));
 
 // Default route for testing server response
-
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
